862504cdf1b06b190ecd860766788105f0081d8f
iamfrisbee <shaun@iamfrisbee.com>
2022-10-22 14:35:49 -0400

    Upgrade to wordpress 6.0.3

--- Dockerfile
+++ Dockerfile
@@ -1,14 +1,24 @@
-FROM php:7.4-apache
+FROM php:7.4-apache as base
 
 # set the default environment variables
 ENV MYSQL_DATABASE wordpress
 ENV MYSQL_USER wordpress
 ENV MYSQL_PASSWORD iekalj4h2e3hi
+ENV PREFIX wp_
 ENV PORT 80
 ENV SSL_PORT 443
 
+ENV DEBIAN_FRONTEND noninteractive
+
+# create directory for mysql database restores
+RUN mkdir -p /src/db
+
 # install WordPress Requirements
-RUN apt-get update -y && apt-get install -y wget gettext imagemagick libmagickwand-dev pwgen libzip-dev
+RUN apt-get update -y && \
+  apt-get install -y sudo wget gettext imagemagick libmagickwand-dev pwgen libzip-dev make libssl-dev libghc-zlib-dev libcurl4-gnutls-dev libexpat1-dev unzip
+
+# enable mod rewrite
+RUN a2enmod rewrite
 
 # install imagick pecl extension
 RUN echo '' | pecl install imagick
@@ -21,15 +31,29 @@ RUN docker-php-source extract \
   && docker-php-ext-install zip \
   && docker-php-source delete
 
+
+FROM base as mysql
+RUN mkdir -p /src/mysql && \
+  cd /src/mysql && \
+  wget https://dev.mysql.com/get/mysql-apt-config_0.8.22-1_all.deb && \
+  apt install -y ./mysql-apt-config_0.8.22-1_all.deb && \
+  apt-get update && apt-get install -y mysql-server
+
+FROM mysql as git
+RUN mkdir -p /src/git && \
+  cd /src/git && \
+  wget https://github.com/git/git/archive/refs/heads/master.zip && \
+  unzip master.zip && \
+  cd git-master && \
+  make prefix=/usr/local all && \
+  make prefix=/usr/local install
+
+FROM git as wordpress
 # set the working directory
 WORKDIR /var/www/html
 
 # copy needed files
 COPY htaccess.txt .htaccess
-COPY docker-entrypoint.sh /usr/local/bin/
-
-# fix permissions
-RUN chmod +x /usr/local/bin/docker-entrypoint.sh
 
 # download wordpress
 RUN wget -O wordpress.tar.gz https://wordpress.org/latest.tar.gz
@@ -48,8 +72,13 @@ RUN rm wordpress.tar.gz \
   && rm -Rf wp-content/themes/twentyseventeen \
   && rm -Rf wp-content/themes/twentynineteen
 
-# enable mod rewrite
-RUN a2enmod rewrite
+FROM wordpress
+
+# add the entry point file
+COPY docker-entrypoint.sh /usr/local/bin/
+
+# fix permissions
+RUN chmod +x /usr/local/bin/docker-entrypoint.sh
 
 # set the entry point
-ENTRYPOINT ["docker-entrypoint.sh"]
+CMD ["docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,20 +1,39 @@
 #!/bin/bash
 
-if [ ! -f .initialized ]; then
+mysqld_safe &
+
+sleep 10
+
+if [ ! -f /src/.initialized ]; then
+
+  # setup the database
+  mysql -e "set global sql_mode = '';"
+  mysql -e "CREATE DATABASE ${MYSQL_DATABASE} /*\!40100 DEFAULT CHARACTER SET utf8 */;"
+  mysql -e "CREATE USER ${MYSQL_USER}@'%' IDENTIFIED BY '${MYSQL_PASSWORD}';"
+  mysql -e "GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}.* TO '${MYSQL_USER}'@'%';"
+  mysql -e "FLUSH PRIVILEGES;"
+
+  # wordpress conflicts
+
+  # restore database file
+  for FILE in /src/db/*; do mysql $MYSQL_DATABASE < $FILE; done
+
+  # configure wordpress
   mv wp-config-sample.php wp-config.php
-  sed -i s/localhost/db/ wp-config.php
+  sed -i s/localhost/127\.0\.0\.1/ wp-config.php
   sed -i s/database_name_here/$MYSQL_DATABASE/ wp-config.php
   sed -i s/username_here/$MYSQL_USER/ wp-config.php
   sed -i s/password_here/$MYSQL_PASSWORD/ wp-config.php
-  sed -i "s/define('AUTH_KEY',\s*'put your unique phrase here');/define('AUTH_KEY', '`pwgen -1 -s 64`');/" wp-config.php
-  sed -i "s/define('SECURE_AUTH_KEY',\s*'put your unique phrase here');/define('SECURE_AUTH_KEY', '`pwgen -1 -s 64`');/" wp-config.php
-  sed -i "s/define('LOGGED_IN_KEY',\s*'put your unique phrase here');/define('LOGGED_IN_KEY', '`pwgen -1 -s 64`');/" wp-config.php
-  sed -i "s/define('NONCE_KEY',\s*'put your unique phrase here');/define('NONCE_KEY', '`pwgen -1 -s 64`');/" wp-config.php
-  sed -i "s/define('AUTH_SALT',\s*'put your unique phrase here');/define('AUTH_SALT', '`pwgen -1 -s 64`');/" wp-config.php
-  sed -i "s/define('SECURE_AUTH_SALT',\s*'put your unique phrase here');/define('SECURE_AUTH_SALT', '`pwgen -1 -s 64`');/" wp-config.php
-  sed -i "s/define('LOGGED_IN_SALT',\s*'put your unique phrase here');/define('LOGGED_IN_SALT', '`pwgen -1 -s 64`');/" wp-config.php
-  sed -i "s/define('NONCE_SALT',\s*'put your unique phrase here');/define('NONCE_SALT', '`pwgen -1 -s 64`');/" wp-config.php
-  echo "define('FS_METHOD', 'direct');" >> wp-config.php
+  sed -i "s/'wp_'/'$WP_PREFIX'/" wp-config.php
+  sed -i "s/define(\s*'AUTH_KEY',\s*'put your unique phrase here'\s*);/define('AUTH_KEY', '`pwgen -1 -s 64`');/" wp-config.php
+  sed -i "s/define(\s*'SECURE_AUTH_KEY',\s*'put your unique phrase here'\s*);/define('SECURE_AUTH_KEY', '`pwgen -1 -s 64`');/" wp-config.php
+  sed -i "s/define(\s*'LOGGED_IN_KEY',\s*'put your unique phrase here'\s*);/define('LOGGED_IN_KEY', '`pwgen -1 -s 64`');/" wp-config.php
+  sed -i "s/define(\s*'NONCE_KEY',\s*'put your unique phrase here'\s*);/define('NONCE_KEY', '`pwgen -1 -s 64`');/" wp-config.php
+  sed -i "s/define(\s*'AUTH_SALT',\s*'put your unique phrase here'\s*);/define('AUTH_SALT', '`pwgen -1 -s 64`');/" wp-config.php
+  sed -i "s/define(\s*'SECURE_AUTH_SALT',\s*'put your unique phrase here'\s*);/define('SECURE_AUTH_SALT', '`pwgen -1 -s 64`');/" wp-config.php
+  sed -i "s/define(\s*'LOGGED_IN_SALT',\s*'put your unique phrase here'\s*);/define('LOGGED_IN_SALT', '`pwgen -1 -s 64`');/" wp-config.php
+  sed -i "s/define(\s*'NONCE_SALT',\s*'put your unique phrase here'\s*);/define('NONCE_SALT', '`pwgen -1 -s 64`');/" wp-config.php
+  echo "define( 'FS_METHOD', 'direct' );" >> wp-config.php
 
   if [ $PORT = "80" ]; then
     echo "define( 'WP_HOME', 'http://localhost' );" >> wp-config.php
@@ -31,7 +50,7 @@ if [ ! -f .initialized ]; then
   sed -i s/80/$PORT/ /etc/apache2/ports.conf
   sed -i s/443/$SSL_PORT/ /etc/apache2/ports.conf
 
-  touch .initialized
+  touch /src/.initialized
 fi
 
 exec apache2-foreground
